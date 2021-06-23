dd0dc847dfb3654bf9337bb039029d5d50ef34fb
Tshepo Bahula <tshepo.bahula@ibm.com>
2021-06-23 23:15:31 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World 🎉 👋🏻');
+});
+
+// start the server
+app.listen(PORT, () => {
+	console.log(`app is listening on port ${PORT} 🦄`);
+});
